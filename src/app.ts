2b57eb3587a9ab9ef203ef1c5618ee0f6eb49ca1
Diorgenes Morais <diorgenesmorais@gmail.com>
2024-06-23 09:23:40 -0300

    feat: development app basic

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log('testing...');
+import express, { Request, Response } from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 4000
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({text: 'Hello, welcome'})
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
