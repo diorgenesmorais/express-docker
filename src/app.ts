8f44d38a4d7dccde504a10085c3db53c6f23f582
Diorgenes Morais <diorgenesmorais@gmail.com>
2024-06-23 10:53:56 -0300

    feat: create zipcode route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
 import express, { Request, Response } from 'express'
+import cep from 'cep-promise'
 
 const app = express()
 const PORT = process.env.PORT || 4000
 
 app.get('/', (req: Request, res: Response) => {
-    res.json({text: 'Hello, welcome'})
+    res.json({text: 'hello, welcome, check a cep on the route "/cep?zipcode=01000000"'})
+})
+
+app.get('/cep', async (req: Request, res: Response) => {
+    const { zipcode } = req.query
+
+    if (!zipcode) {
+        res.status(400).json({error: 'You need to pass the zipcode as a parameter in the url'})
+        return
+    }
+
+    const result = await cep(zipcode.toString())
+
+    res.status(200).json(result)
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
